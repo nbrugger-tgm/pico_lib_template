5719841c28b48e5cc89937492df29389190ab279
nbrugger-tgm <nbrugger@student.tgm.ac.at>
2021-03-03 10:17:45 +0100

    fix: add build script check for SDK

--- compile.sh
+++ compile.sh
@@ -1,3 +1,10 @@
+#/bin/sh
+if [ -n "$PICO_SDK_PATH"] && [ ! -d "$PICO_SDK_PATH" ] 
+then
+	echo "environment variable PICO_SDK_PATH not found. Please setup the pico-sdk regarding to the README.md"
+	exit 1
+fi
+
 mkdir build -p
 cd build
 cmake ..
